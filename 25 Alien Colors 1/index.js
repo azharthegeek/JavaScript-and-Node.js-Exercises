30bb3d8b27fb61613f41abfcf426753a44c7ba6f
Muhammad Azhar <azharthegeek@gmail.com>
2022-10-04 02:40:51 +0500

    Update

---
+++ 25 Alien Colors 1/index.js
@@ -0,0 +1,13 @@
+//Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+var alien_color = "green"
+
+
+//If it is, print a message that the player just earned 5 points.
+if (alien_color == "green") {
+    console.log("You Earned 5 Points")
+}
+
+if (alien_color == "yellow" || alien_color == "red") {
+
+    //Fail
+}
